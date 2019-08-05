e726f68b9626aa31ef641852c003c88edb2d3efa
Daoud Clarke <daoud.clarke@gmail.com>
2019-08-05 22:14:29 +0100

    Add the package

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='pastry',
+    name='pypastry',
     version='0.0.1',
     description='PyPastry machine learning experimentation framework',
     author='Daoud Clarke',
     scripts=['pastry'],
-    install_requires=['tomlkit', 'pandas', 'scikit-learn']
+    install_requires=['tomlkit', 'pandas', 'scikit-learn'],
+    packages=['pypastry'],
 )
