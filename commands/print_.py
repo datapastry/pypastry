c87b0b0e1cc6f7482d2235973399e1e65cec8180
Daoud Clarke <daoud.clarke@gmail.com>
2019-08-09 16:09:18 +0100

    Refactor to move core content to pypastry

--- commands/print_.py
+++ commands/print_.py
@@ -1,32 +1,5 @@
-import json
-from glob import glob
-
-from git import Repo
-from pandas import DataFrame
-from pypastry import RESULTS_GLOB
+from pypastry.display import print_results
 
 
-def print_results():
-    results = []
-
-    repo = Repo('.')
-
-    for path in glob(RESULTS_GLOB):
-        with open(path) as results_file:
-
-            git_hash = next(repo.iter_commits(paths=path)).hexsha[:8]
-            result_json = json.load(results_file)
-            result = {
-                'Git hash': git_hash,
-                'Dataset hash': result_json['dataset']['hash'][:8],
-                'Run start': result_json['run_start'][:19],
-                'Model': result_json['model_info']['type'],
-                'Score': "{:.3f} ± {:.3f}".format(result_json['results']['test_score'],
-                                            result_json['results']['test_score_sem']),
-                'Duration (s)': "{:.2f}".format(result_json['run_seconds']),
-            }
-            results.append(result)
-
-    results.sort(key=lambda row: row['Run start'])
-    results_dataframe = DataFrame(results)
-    print(results_dataframe)
+def run():
+    print_results()
---
+++ pypastry/evaluation.py
@@ -0,0 +1,78 @@
+import json
+from datetime import datetime
+from os import mkdir
+from tempfile import NamedTemporaryFile
+
+import pandas as pd
+from git import Repo
+from pypastry.display import print_results
+from pypastry.hasher import get_dataset_hash
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import cross_validate
+
+
+def run_experiment(experiment):
+    print("Got dataset with {} rows".format(len(experiment.dataset)))
+    repo = Repo('.')
+    if repo.is_dirty():
+        _run_evaluation(experiment.cross_validator, experiment.dataset,
+                        experiment.label_column, experiment.predictor, repo, experiment.scorer)
+    else:
+        print("Clean repo, nothing to do")
+    print_results()
+
+
+def _run_evaluation(cross_validator, dataset, label_column, predictor, repo, scorer):
+    X = dataset.drop(columns=[label_column])
+    y = dataset[label_column]
+    predictors = [predictor]
+    run_infos = evaluate_predictors(X, predictors, y, cross_validator, scorer)
+    repo.git.add(update=True)
+    dataset_hash = get_dataset_hash(dataset)
+    dataset_info = {
+        'hash': dataset_hash,
+        'columns': dataset.columns.tolist(),
+    }
+    try:
+        mkdir('results')
+    except FileExistsError:
+        pass
+    for i, run_info in enumerate(run_infos):
+        run_info['dataset'] = dataset_info
+        with NamedTemporaryFile(mode='w', prefix='result-', suffix='.json', dir='results', delete=False) as output_file:
+            json.dump(run_info, output_file, indent=4)
+            output_file.flush()
+            repo.index.add([output_file.name])
+    repo.index.commit('Add results')
+
+
+def evaluate_predictors(X, predictors, y, cross_validator, scorer):
+    run_infos = []
+    for predictor in predictors:
+        start = datetime.utcnow()
+        scores_dict = cross_validate(predictor, X, y, cv=cross_validator, scoring=scorer)
+        end = datetime.utcnow()
+
+        scores = pd.DataFrame(scores_dict)
+        mean_scores = scores.mean()
+        sem_scores = scores.sem()
+        results = dict(mean_scores.items())
+        results.update({k + '_sem': v for k, v in sem_scores.items()})
+
+        model_info = get_model_info(predictor)
+
+        run_info = {
+            'run_start': str(start),
+            'run_end': str(end),
+            'run_seconds': (end - start).total_seconds(),
+            'results': results,
+            'model_info': model_info,
+        }
+        run_infos.append(run_info)
+    return run_infos
+
+
+def get_model_info(model: BaseEstimator):
+    info = model.get_params()
+    info['type'] = type(model).__name__
+    return info
