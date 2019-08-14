787c709c0c97343e4704b749c5298c784a7bc4e6
Daoud Clarke <daoud.clarke@gmail.com>
2019-08-14 07:57:21 +0100

    Run experiment in the test

--- test/evaluation_test.py
+++ test/evaluation_test.py
@@ -1,9 +1,12 @@
+from unittest.mock import Mock
+
 from pandas import DataFrame
 from sklearn.metrics import accuracy_score, make_scorer
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.tree import DecisionTreeClassifier
 
 from pypastry.experiment import Experiment
+from pypastry.experiment.evaluation import ExperimentRunner
 
 
 def test_simple_evaluation():
@@ -20,6 +23,14 @@ def test_simple_evaluation():
 
     experiment = Experiment(dataset, 'b', predictor, cross_validation, scorer)
 
-    # TODO: allow passing in a git repo here so that we can easily mock it
-    # TODO: also pass in a results creator so that we don't actually create results in tests
-    # TODO: pass in a display class that allows no caching
+    git_mock = Mock()
+    results_repo_mock = Mock()
+    results_display_mock = Mock()
+    runner = ExperimentRunner(git_mock, results_repo_mock, results_display_mock)
+
+    runner.run_experiment(experiment, False, "Test commit message")
+
+    # TODO: check the results
+    # TODO: check that git commit is called
+    # TODO: check that the display is cached
+    print(results_repo_mock.save_results.call_args_list)
