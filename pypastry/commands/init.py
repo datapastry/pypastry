d4f614cc1e7e4d56b2474e65ef868ebe056ade9e
MarcoGorelli <m.e.gorelli@gmail.com>
2019-09-18 19:59:34 +0100

    Add mypy.ini with ignore missing imports, untyped defs, more precise type in display

--- pypastry/commands/init.py
+++ pypastry/commands/init.py
@@ -9,14 +9,15 @@ from git import Repo
 def run():
     parser = argparse.ArgumentParser(prog='pastry init')
     parser.add_argument('directory', type=str, help='Path to directory to create (or default to current directory)')
-    args = parser.parse_args(sys.argv[2:])
+    args = parser.parse_args(sys.argv[2:])  # type: argparse.Namespace
 
     if args.directory is not None:
         mkdir(args.directory)
         chdir(args.directory)
 
-    repo = Repo.init('.')
+    repo = Repo.init('.')  #type:  git.repo.base.Repo
     for filename in ['pie.py', '.gitignore']:
         source_file_path = path.join(path.dirname(__file__), '..', '..', 'data', filename)
         copyfile(source_file_path, filename)
         repo.index.add([filename])
+
