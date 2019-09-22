e6b15b896fa6f6c96191edc0bdb759697eb0fdc7
MrRutledge <rahimrutledge@gmail.com>
2019-09-22 19:00:24 +0100

    Updating the Setup file for Pypi packaging

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from setuptools import setup
 if sys.version_info < (3, 5):
     sys.exit('Sorry, PyPastry requires Python version 3.5 or greater')
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pypastry',
     version='0.0.1',
@@ -12,5 +15,13 @@ setup(
     author='Daoud Clarke',
     scripts=['pastry'],
     install_requires=['tomlkit', 'pandas', 'scikit-learn', 'pyarrow', 'gitpython'],
-    packages=['pypastry'],
+    package_dir ={'': 'pypastry'},
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
 )
