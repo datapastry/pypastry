79f9ed8f2b223519ac9ca0d6d8d7b7db53ba9164
Filip Langr <flangr@luc.id>
2021-05-17 14:12:27 +0800

    Removed unnecessary code

--- pypastry/experiment/results.py
+++ pypastry/experiment/results.py
@@ -3,7 +3,7 @@ import os
 import glob
 from pathlib import Path
 from tempfile import NamedTemporaryFile
-from typing import Dict, Any, List, NamedTuple
+from typing import Dict, Any, NamedTuple
 
 
 Result = NamedTuple('Result', [('data', Dict[str, Any]), ("dirty", bool)])
@@ -12,7 +12,6 @@ Result = NamedTuple('Result', [('data', Dict[str, Any]), ("dirty", bool)])
 class ResultsRepo:
     def __init__(self, results_path: str):
         self.results_path = results_path
-        self.result_files = []
 
     def save_results(self, run_info: Dict[str, Any], dataset_info: Dict[str, Any], git_info: Dict[str, str]):
         try:
@@ -27,10 +26,9 @@ class ResultsRepo:
             run_info["result_json_name"] = Path(output_file.name).name
             json.dump(run_info, output_file, indent=4, default=str)
             output_file.flush()
-        self.result_files.append(output_file.name)
 
     def get_results(self, git_repo):
-        for path in sorted(glob.glob(os.path.join(self.results_path, "*.json")), key=os.path.getmtime):
+        for path in glob.glob(os.path.join(self.results_path, "*.json")):
             with open(str(path), "r") as results_file:
                 result_json = json.load(results_file)
             yield Result(result_json, git_repo.is_dirty())
