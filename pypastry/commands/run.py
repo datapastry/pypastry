f4f1549dfb83b3e7773758c08c3d558313b1dd8b
Filip Langr <flangr@luc.id>
2021-06-16 13:56:21 +0800

    Experiment run returns path to result JSON. Printing results can be disabled.

--- pypastry/commands/run.py
+++ pypastry/commands/run.py
@@ -8,6 +8,7 @@ def run():
     parser = argparse.ArgumentParser(prog='pastry run')
     parser.add_argument('-m', '--message', default="", type=str, help='Summary message about the experiment.')
     parser.add_argument('-f', '--force', action='store_true', help='Force a re-run of the experiment')
+    parser.add_argument('-p', '--no-print', action='store_true', help='Do not print results.')
 
     args = parser.parse_args(sys.argv[2:])
 
@@ -17,4 +18,4 @@ def run():
     force = args.force
     message = args.message
 
-    run_experiment(experiment, message, force)
+    run_experiment(experiment, message, force, show_results=not args.no_print)
